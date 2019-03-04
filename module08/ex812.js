84b227add5f856f6a5aa312c0e6507f883471221
GeoM <scarlll@hotmail.com>
2019-03-04 12:41:49 +0200

    Primitive Shapes - ellipse

--- module08/ex812.js
+++ module08/ex812.js
@@ -3,9 +3,11 @@ function setup() {
 	// create canvas
 	createCanvas(800, 500);
 	// set background color
-	background('#31bc33');
+	// background('#31bc33');
 }
 
 function draw() {
+	background('#31bc33');
 
+	ellipse(width/2,height/2,60);
 }
