abc76cb5e23a60c65ecffc1cb862a0cb6313e5b5
GeoM <scarlll@hotmail.com>
2019-03-04 12:54:08 +0200

    Primitive Shapes - line

---
+++ module08/ex824.js
@@ -0,0 +1,24 @@
+let i = 20;
+
+function setup() {
+
+    // create canvas
+    createCanvas(800, 600);
+    // set background color
+    background('orange');
+}
+
+function draw() {
+
+    // line((width / 2 - 100), height / 2, (width / 2 + 100), height / 2);
+    // line((width / 2 - 100), height / 2, (width / 2 - 200), 150);
+    // line((width / 2 + 100), height / 2, (width / 2 + 200), 350);
+
+    line(350, 100, 350, 300);
+    // right line
+    line(450, 100, 450, 300);
+    // draw horizontal lines across the height of the two vertical lines
+    for (i; i < 200; i = i + 20) {
+        line(350, 100 + i, 450, 100 + i);
+    }
+}
